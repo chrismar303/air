206782777898d0dc6ddf0d46a993390b07cf57fa
chrismar303 <chrismar303@gmail.com>
2023-03-05 18:40:53 -0800

    Guest Screen search button

--- src/screens/Guests/index.jsx
+++ src/screens/Guests/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import GuestRow from './GuestRow'
 import styles from './styles'
 
 const GuestsScreen = props => {
   return (
-    <View>
-      <View style={styles.rows}>
+    <View style={styles.guestContainer}>
+      <View>
         <GuestRow type="Adults" caption="Ages 13 or above" style={styles.row}/>
         <GuestRow type="Children" caption="Ages 2 - 12" style={styles.row}/>
         <GuestRow type="Infants" style={styles.row}/>
       </View>
+      <Pressable style={styles.searchButton}>
+        <Text style={styles.searchButtonText}>Search</Text>
+      </Pressable>
     </View>
   )
 }
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { View, Text, ImageBackground, Pressable } from 'react-native'
 import styles from '@/screens/Home/styles.js'
 import Fontisto from 'react-native-vector-icons/Fontisto'
-import { useNavigation } from '@react-navigation/core'
 const image = require('@/assets/images/israel.jpg')
 
 const HomeScreen = props => {
-  const navigation = useNavigation()
 
   return (
     <View style={styles.container}>
@@ -17,7 +15,6 @@ const HomeScreen = props => {
       >
         <Pressable 
           style={styles.searchButton}
-          onPress={() => navigation.navigate('Search')}
         >
           <Fontisto name="search" size={25} color={"#F15454"} />
           <Text style={styles.searchButtonText}>Where to next traveler!</Text>
